0f530ddcc6d1568baacd0097f48728dad14b8bd1
Miskat <misky0310@gmail.com>
2024-02-25 16:30:13 +0530

    Project Start and already created the navbar,hero and cards section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark">
+      <div className="nav w-full relative">
+        <Navbar />
+      </div>
+      <body suppressHydrationWarning={true} className={inter.className}>{children}</body>
     </html>
   );
 }
---
+++ src/components/FeaturedCourses.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import { BackgroundGradient } from "@/components/ui/background-gradient";
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function FeaturedCourses({course}:any) {
+  return (
+    <div>
+      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200 text-center">
+          {course.title}
+        </p>
+
+        <p className="text-sm text-neutral-600 dark:text-neutral-400 text-center md:h-[40%]">
+          {course.description}
+        </p>
+        <Link href={`/${course.slug}`}>
+            <button className="rounded-full pl-4 pr-1 py-3 text-white flex justify-center items-center space-x-1 bg-black mt-4 text-sm font-bold dark:bg-zinc-800 w-1/2 mx-auto">
+            Learn More
+            </button>
+        </Link>
+      </BackgroundGradient>
+    </div>
+  );
+}
